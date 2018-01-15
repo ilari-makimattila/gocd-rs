529eec8aaaa7a7b9f79c9d04053e049ce7cde26e
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2018-01-15 20:05:04 +0100

    Add functionality for getting pipeline groups

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
+#[cfg(test)] extern crate mockito;
+extern crate reqwest;
 #[macro_use] extern crate serde_derive;
 
+use std::fmt;
 
-mod pipelines;
+use reqwest::header::{Accept, UserAgent, qitem};
 
 
-struct Gocd {
+pub mod pipelines;
+
+type Result<T> = ::std::result::Result<T, Error>;
+
+
+pub struct Error {}
+
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Gocd error")
+    }
+}
+
+pub struct Gocd {
     base_url: String,
     username: String,
     password: String
@@ -17,6 +34,34 @@ impl Gocd {
             base_url: String::from(base_url),
             username: String::from(username),
             password: String::from(password)
+        }
+    }
+
+    pub fn get(&self, url: &str, accept: Option<&str>) -> Result<String> {
+        let client = reqwest::Client::new();
+        let url = format!("{}/{}", self.base_url, url);
+        let username = self.username.clone();
+        let password = Some(self.password.clone());
+        
+        let accept = match accept {
+            Some(a) => a,
+            None => "application/json"
+        };
+
+        let result = client.get(&url)
+            .basic_auth(username, password)
+            .header(UserAgent::new(format!("Gocd-rs/{} ({})", 
+                                           env!("CARGO_PKG_VERSION"),
+                                           env!("CARGO_PKG_HOMEPAGE"))))
+            .header(Accept(vec![qitem(accept.parse().unwrap())]))
+            .send();
+
+        match result {
+            Ok(mut r) => match r.text() {
+                Ok(t) => Ok(t),
+                Err(e) => Err(Error {})
+            },
+            Err(e) => Err(Error {}) // todo error reporting
         }
     }
 }
--- src/pipelines/mod.rs
+++ src/pipelines/mod.rs
@@ -1,23 +1,41 @@
 extern crate serde;
 extern crate serde_json;
 
+use super::{Gocd, Result};
 
-#[derive(Deserialize)]
-struct Pipeline {
+
+#[derive(Deserialize, Debug)]
+pub struct Pipeline {
     pub name: String,
     pub label: String,
     pub stages: Vec<PipelineStage>
 }
 
-#[derive(Deserialize)]
-struct PipelineStage {
+#[derive(Deserialize, Debug)]
+pub struct PipelineStage {
     pub name: String
 }
 
-#[derive(Deserialize)]
-struct PipelineGroup {
+#[derive(Deserialize, Debug)]
+pub struct PipelineGroup {
     pub name: String,
     pub pipelines: Vec<Pipeline>
+}
+
+
+pub trait Pipelines {
+    fn pipeline_groups(&self) -> Result<Vec<PipelineGroup>>;
+}
+
+impl Pipelines for Gocd {
+    fn pipeline_groups(&self) -> Result<Vec<PipelineGroup>> {
+        let data = self.get("go/api/config/pipeline_groups", 
+                            Some("application/json"));
+        match data {
+            Ok(data) => Ok(serde_json::from_str(data.as_str()).unwrap()),
+            Err(e) => Err(e)
+        }
+    }
 }
 
 
@@ -25,7 +43,10 @@ struct PipelineGroup {
 mod tests {
     extern crate serde_json;
 
+    use mockito::mock;
+
     use super::*;
+    use super::super::*;
 
     #[test]
     fn pipeline_group_deserializes() {
@@ -36,4 +57,20 @@ mod tests {
         assert_eq!(pipeline_group[0].pipelines[0].name, "up42");
         assert_eq!(pipeline_group[0].pipelines[0].stages[0].name, "up42_stage");
     }
+
+    #[test]
+    fn it_fetches_pipelines() {
+        let _m = mock("GET", "/go/api/config/pipeline_groups")
+            .with_status(200)
+            .with_body(include_str!("../../tests/data/pipeline_groups.json"))
+            .create();
+
+        let gocd = Gocd::new(mockito::SERVER_URL, "foo", "bar");
+        let pipeline_groups = gocd.pipeline_groups().unwrap();
+
+        assert_eq!(pipeline_groups[0].name, "first");
+        assert_eq!(pipeline_groups[0].pipelines[0].name, "up42");
+        assert_eq!(pipeline_groups[0].pipelines[0].stages[0].name, "up42_stage");
+    }
 }
+
