1f5470bb18097efa36d89a91c4db3204cdcbc022
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2018-01-25 10:31:24 +0100

    Add trait for getting stage history

--- src/stages/history.rs
+++ src/stages/history.rs
@@ -31,11 +31,32 @@ pub struct StageHistoryJob {
 }
 
 
+pub trait StageHistories {
+    fn stage_history(&self, pipeline: &str, stage_name: &str) -> Result<Vec<StageHistory>>;
+}
+
+impl StageHistories for Gocd {
+    fn stage_history(&self, pipeline: &str, stage_name: &str) -> Result<Vec<StageHistory>> {
+        let data = self.get(format!("go/api/stages/{}/{}/history", pipeline, stage_name).as_str(),
+                            Some("application/json"));
+        match data {
+            Ok(data) => {
+                let pagination: Pagination = serde_json::from_str(data.as_str()).unwrap();
+                Ok(pagination.stages)
+            },
+            Err(e) => Err(e)
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     extern crate serde_json;
 
+    use mockito::mock;
     use super::*;
+    use super::super::super::*;
 
     #[test]
     fn it_deserializes_from_example() {
@@ -46,5 +67,19 @@ mod tests {
         assert_eq!(stage_history[0].name, "defaultStage");
         assert_eq!(stage_history[0].counter, "1");
         assert_eq!(stage_history[0].jobs[0].name, "defaultJob");
+    }
+
+    #[test]
+    fn it_fetches_stage_history() {
+        let _m = mock("GET", "/go/api/stages/pipeline1/stage1/history")
+            .with_status(200)
+            .with_body(include_str!("../../tests/data/stage_history.json"))
+            .create();
+
+        let gocd = Gocd::new(mockito::SERVER_URL, "foo", "bar");
+        let stage_history = gocd.stage_history("pipeline1", "stage1").unwrap();
+
+        assert_eq!(stage_history[0].name, "defaultStage");
+        assert_eq!(stage_history[0].pipeline_name, "mypipeline");
     }
 }
