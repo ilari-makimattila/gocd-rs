84e7c8e1a5eb8bfb0286b8d55741f3e284cb8825
Ilari Mäkimattila <ilari.makimattila@gmail.com>
2018-02-13 10:01:20 +0100

    Use pipeline group example to calculate pipeline and stage counts

--- examples/pipeline_groups.rs
+++ examples/pipeline_groups.rs
@@ -11,7 +11,14 @@ fn main() {
                          env::var("GO_USERNAME").unwrap().as_str(),
                          env::var("GO_PASSWORD").unwrap().as_str());
 
-    let pipelines = gocd.pipeline_groups();
+    let pipelines = gocd.pipeline_groups().unwrap();
 
-    println!("{:?}", pipelines.unwrap());
+    let pipeline_group_count = pipelines.len();
+    let pipeline_count = pipelines.iter().fold(0, |acc, p| acc + p.pipelines.len());
+    let stage_count = pipelines.iter()
+        .fold(0, |acc, pipeline| acc + pipeline.pipelines.iter().fold(0, |sa, p| sa + p.stages.len()));
+
+    println!("groups: {}", &pipeline_group_count);
+    println!("pipelines: {}", &pipeline_count);
+    println!("stages: {}", &stage_count);
 }
